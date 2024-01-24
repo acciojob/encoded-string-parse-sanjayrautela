78e99351ac7fb99530c1d4831ae141818ce6cacd
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-01-24 21:04:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function parseCode(encodedString) {
     // Split the input string using '000' as the separator
-    const [firstName, lastName, id] = encodedString.split('000');
+    const [firstName, lastName, id] = encodedString.split('/0+/');
 
     // Create and return an object with the extracted values
     return {
